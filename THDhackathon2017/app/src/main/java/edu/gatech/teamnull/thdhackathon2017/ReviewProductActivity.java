aeaf736bbb1c04aea01daf05f650b8721e3902d9
Colby Tobin <colby@Colbys-MacBook-Pro.local>
2017-09-17 07:05:28 -0400

    hh

--- THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/ReviewProductActivity.java
+++ THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/ReviewProductActivity.java
@@ -67,14 +67,20 @@ public class ReviewProductActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // app icon in action bar clicked; goto parent activity.
-                this.finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        } else if(id==android.R.id.home)
+        {
+            finish();
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 }
