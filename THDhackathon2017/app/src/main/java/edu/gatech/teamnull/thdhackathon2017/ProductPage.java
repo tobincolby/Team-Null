05aaf31f8d10d3468d1080a52294fae60733ffab
lucas965 <lucasnativio@gmail.com>
2017-09-17 09:10:21 -0400

    Optimized import statments for ProductPage

--- THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/ProductPage.java
+++ THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/ProductPage.java
@@ -1,15 +1,11 @@
 package edu.gatech.teamnull.thdhackathon2017;
 
 import android.content.Intent;
-import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -18,12 +14,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.util.ArrayList;
 
 import edu.gatech.teamnull.thdhackathon2017.model.*;
 
