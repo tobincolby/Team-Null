043e4c91398958c366b17c053d1b59b1c8504e8f
Colby Tobin <colby@Colbys-MacBook-Pro.local>
2017-09-16 22:32:49 -0400

    Set up barcode scanner for scanning images you take from camera

--- THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/BarcodeScannerActivity.java
+++ THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/BarcodeScannerActivity.java
@@ -1,5 +1,7 @@
 package edu.gatech.teamnull.thdhackathon2017;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
@@ -7,6 +9,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
@@ -18,26 +21,18 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class BarcodeScannerActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private BarcodeDetector detector;
+    private TextView txtView;
+    private ImageView myImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode_scanner);
 
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
-        TextView txtView = (TextView) findViewById(R.id.txtContent);
-
-        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
-        Bitmap myBitmap = BitmapFactory.decodeResource(
-                getApplicationContext().getResources(),
-                R.drawable.puppy);
-        myImageView.setImageBitmap(myBitmap);
-
-        BarcodeDetector detector =
+        detector =
                 new BarcodeDetector.Builder(getApplicationContext())
                         .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
                         .build();
@@ -46,9 +41,38 @@ public class BarcodeScannerActivity extends AppCompatActivity {
             return;
         }
 
-        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-        SparseArray<Barcode> barcodes = detector.detect(frame);
-        Barcode thisCode = barcodes.valueAt(0);
-        txtView.setText(thisCode.rawValue);
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        txtView = (TextView) findViewById(R.id.txtContent);
+        myImageView = (ImageView) findViewById(R.id.imgview);
+
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            myImageView.setImageBitmap(imageBitmap);
+
+            Frame frame = new Frame.Builder().setBitmap(imageBitmap).build();
+            SparseArray<Barcode> barcodes = detector.detect(frame);
+            Barcode thisCode = barcodes.valueAt(0);
+            txtView.setText(thisCode.rawValue);
+            Log.d("FINE", "Works");
+        }
     }
 }
