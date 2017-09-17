5538a40234efc346faf3c45501805d75fb84845b
Karan Achtani <Karan.achtani@gatech.edu>
2017-09-16 21:09:55 -0400

    added youtube video page

---
+++ THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/model/Config.java
@@ -0,0 +1,13 @@
+package edu.gatech.teamnull.thdhackathon2017.model;
+
+/**
+ * Created by karanachtani on 9/16/17.
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCyZYbZPxJhH-yze8kJpKx_7wDsg6Cz8Pw";
+}
