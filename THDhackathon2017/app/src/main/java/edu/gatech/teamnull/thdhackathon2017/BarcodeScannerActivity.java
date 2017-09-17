47ef8c9cec678731dac6af40c252984d0522bc0b
karshinlin <lin.karshin@gmail.com>
2017-09-17 04:19:47 -0400

    Fixed xml mistakes.

--- THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/BarcodeScannerActivity.java
+++ THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/BarcodeScannerActivity.java
@@ -161,9 +161,6 @@ public class BarcodeScannerActivity extends AppCompatActivity implements Navigat
             startActivity(i);
         } else if (id == R.id.nav_account) {
 
-        } else if (id == R.id.nav_selected_product) {
-            Intent i = new Intent(this, SelectedProductPage.class);
-            startActivity(i);
         } else if (id == R.id.nav_products){
             Intent i = new Intent(this, ProductPage.class);
             startActivity(i);
