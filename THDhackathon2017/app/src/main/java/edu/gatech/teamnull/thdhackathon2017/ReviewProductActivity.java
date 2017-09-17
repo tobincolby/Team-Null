7985758ecaab971d877338592a363dc0e91804e1
Colby Tobin <colby@Colbys-MacBook-Pro.local>
2017-09-17 07:17:45 -0400

    Make async task quicker

--- THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/ReviewProductActivity.java
+++ THDhackathon2017/app/src/main/java/edu/gatech/teamnull/thdhackathon2017/ReviewProductActivity.java
@@ -62,6 +62,7 @@ public class ReviewProductActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home_page, menu);
         return true;
     }
